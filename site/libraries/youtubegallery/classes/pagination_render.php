b06a4ab5251094f56b89e69cb7fd78d0ac39da78
joomlaboat <ivankomlev@gmail.com>
2025-03-20 18:37:03 -0800

    CT Pagination clas used instead of Joomla. Should help with the compatibility.

--- site/libraries/youtubegallery/classes/pagination_render.php
+++ site/libraries/youtubegallery/classes/pagination_render.php
@@ -7,7 +7,7 @@
  **/
 
 // No direct access to this file
-
+/*
 namespace YouTubeGallery;
 
 defined('_JEXEC') or die('Restricted access');
@@ -15,29 +15,22 @@ defined('_JEXEC') or die('Restricted access');
 use JESPagination;
 use \Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
-use \YGPagination;
+
+//use \YGPagination;
 
 class Pagination
 {
-    public static function Pagination(&$theme_row, $the_gallery_list, $width, $total_number_of_rows)
-    {
-        $mainframe = Factory::getApplication();
+	public static function Pagination(&$theme_row, $the_gallery_list, $width, $total_number_of_rows)
+	{
+		$mainframe = Factory::getApplication();
 
-        if (((int)$theme_row->es_customlimit) == 0) {
-            //limit=0; // UNLIMITED
-            //No pagination - all items shown
-            return '';
-        } else
-            $limit = (int)$theme_row->es_customlimit;
 
-        $limitstart = Factory::getApplication()->input->getInt('ygstart', 0);
+		$pagination = Pagination::getPagination($total_number_of_rows, $limitstart, $limit, $theme_row);
 
-        $pagination = Pagination::getPagination($total_number_of_rows, $limitstart, $limit, $theme_row);
+		$paginationcode = '';
 
-        $paginationcode = '';
-
-        if ($limit == 0) {
-            $paginationcode .= '
+		if ($limit == 0) {
+			$paginationcode .= '
 				<table cellspacing="0" style="padding:0px;width:' . $width . 'px;border-style: none;"  border="0" >
 				<tr style="height:30px;border-style: none;border-width:0px;">
 				<td style="text-align:left;width:140px;vertical-align:middle;border: none;">' . Text::_('SHOW') . ': ' . $pagination->getLimitBox("") . '</td>
@@ -45,20 +38,22 @@ class Pagination
 				</tr>
 				</table>
 				';
-        } else {
-            $paginationcode .= '<div class="pagination">' . $pagination->getPagesLinks() . '</div>';
-        }
+		} else {
+			$paginationcode .= '<div class="pagination">' . $pagination->getPagesLinks() . '</div>';
+		}
 
-        return $paginationcode;
-    }
+		return $paginationcode;
+	}
 
-    public static function getPagination($num, $limitstart, $limit, &$theme_row)
-    {
-        $AddAnchor = false;
-        if ($theme_row->es_openinnewwindow == 2 or $theme_row->es_openinnewwindow == 3) {
-            $AddAnchor = true;
-        }
+	public static function getPagination($num, $limitstart, $limit, &$theme_row)
+	{
+		$AddAnchor = false;
+		if ($theme_row->es_openinnewwindow == 2 or $theme_row->es_openinnewwindow == 3) {
+			$AddAnchor = true;
+		}
 
-        return new JESPagination($num, $limitstart, $limit, '', $AddAnchor);
-    }
+		return new JESPagination($num, $limitstart, $limit, '', $AddAnchor);
+	}
 }
+
+*/
--- site/libraries/youtubegallery/loader.php
+++ site/libraries/youtubegallery/loader.php
@@ -6,47 +6,43 @@ defined('JPATH_PLATFORM') or die;
 
 function YGLoadClasses(): void
 {
-    $CustomTables_path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
-    $loader_file = $CustomTables_path . 'loader.php';
-    $component_name = 'com_customtables';
+	$CustomTables_path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
+	$loader_file = $CustomTables_path . 'loader.php';
+	$component_name = 'com_customtables';
 
-    if (!file_exists($loader_file)) {
-        $CustomTables_path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_youtubegallery' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
-        $loader_file = $CustomTables_path . 'loader.php';
-        $component_name = 'com_youtubegallery';
-    }
+	if (!file_exists($loader_file)) {
+		$CustomTables_path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_youtubegallery' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
+		$loader_file = $CustomTables_path . 'loader.php';
+		$component_name = 'com_youtubegallery';
+	}
 
-    require_once($loader_file);
+	require_once($loader_file);
 
-    $params = ComponentHelper::getParams($component_name);
-    $loadTwig = $params->get('loadTwig') ?? true;
-    CustomTablesLoader($include_utilities = true, false, null, $component_name, $loadTwig);
+	$params = ComponentHelper::getParams($component_name);
+	$loadTwig = $params->get('loadTwig') ?? true;
+	CustomTablesLoader($include_utilities = true, false, null, $component_name, $loadTwig);
 
-    $path = 'classes' . DIRECTORY_SEPARATOR;
+	$path = 'classes' . DIRECTORY_SEPARATOR;
 
-    require_once($path . 'data.php');
-    require_once($path . 'db.php');
-    require_once($path . 'misc.php');
-    require_once($path . 'gallery_list.php');
-    require_once($path . 'hotplayer.php');
-    require_once($path . 'layoutrenderer.php');
-    require_once($path . 'misc.php');
-    require_once($path . 'pagination_render.php');
-    require_once($path . 'players.php');
-    require_once($path . 'render.php');
-    require_once($path . 'render_css.php');
-    require_once($path . 'responsive_js.php');
-    require_once($path . 'socialbuttons.php');
-    require_once($path . 'thumbnails.php');
+	require_once($path . 'data.php');
+	require_once($path . 'db.php');
+	require_once($path . 'misc.php');
+	require_once($path . 'gallery_list.php');
+	require_once($path . 'hotplayer.php');
+	require_once($path . 'layoutrenderer.php');
+	require_once($path . 'misc.php');
+	require_once($path . 'pagination_render.php');
+	require_once($path . 'players.php');
+	require_once($path . 'render.php');
+	require_once($path . 'render_css.php');
+	require_once($path . 'responsive_js.php');
+	require_once($path . 'socialbuttons.php');
+	require_once($path . 'thumbnails.php');
 
-    //$paginationPath = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_youtubegallery' . DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'pagination.php';
-    //if (file_exists($paginationPath))
-    //require_once($paginationPath);
-
-    //require_once($path . 'dailymotion.php');
-    //require_once($path . 'soundcloud.php');
-    //require_once($path . 'ustream.php');
-    //require_once($path . 'ustreamlive.php');
-    //require_once($path . 'vimeo.php');
-    require_once($path . 'providers' . DIRECTORY_SEPARATOR . 'youtube.php');
+	//require_once($path . 'dailymotion.php');
+	//require_once($path . 'soundcloud.php');
+	//require_once($path . 'ustream.php');
+	//require_once($path . 'ustreamlive.php');
+	//require_once($path . 'vimeo.php');
+	require_once($path . 'providers' . DIRECTORY_SEPARATOR . 'youtube.php');
 }
